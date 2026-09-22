326f47cc52ef0e41b880e1e6fbdfbd54da2a45aa
Raphaeljdk <raphaelfreitasdossantos651@gmail.com>
2026-09-22 13:55:22 -0300

    Add src/proxy.ts

---
+++ src/proxy.ts
@@ -0,0 +1,31 @@
+import { NextResponse, type NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+import { allowedPages } from "@/lib/permissions";
+export async function proxy(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+  const path = request.nextUrl.pathname.split("/")[1] || "dashboard";
+  if (!token) {
+    const url = new URL("/login", request.url);
+    return NextResponse.redirect(url);
+  }
+  if (!allowedPages[token.role]?.includes(path))
+    return NextResponse.redirect(new URL("/comandas", request.url));
+  return NextResponse.next();
+}
+export const config = {
+  matcher: [
+    "/",
+    "/dashboard/:path*",
+    "/clientes/:path*",
+    "/veiculos/:path*",
+    "/servicos/:path*",
+    "/comandas/:path*",
+    "/agendamentos/:path*",
+    "/funcionarios/:path*",
+    "/relatorios/:path*",
+    "/configuracoes/:path*",
+  ],
+};
