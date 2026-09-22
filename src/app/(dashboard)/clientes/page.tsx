1fcc8864e306af2169c2b18a76dd45b89d1ab5bd
Raphaeljdk <raphaelfreitasdossantos651@gmail.com>
2026-09-22 13:45:52 -0300

    Add src/app/(dashboard)/clientes/page.tsx

---
+++ src/app/(dashboard)/clientes/page.tsx
@@ -0,0 +1,9 @@
+import { Suspense } from "react";
+import { EntityPage } from "@/components/entities";
+export default function Page() {
+  return (
+    <Suspense fallback={<p>Carregando...</p>}>
+      <EntityPage kind="clientes" />
+    </Suspense>
+  );
+}
